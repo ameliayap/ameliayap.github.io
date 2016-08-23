95fc5c81fd95d071c2402f89f1c2fc10f079311b
Amelia Yap <amelia.yap@enernoc.com>
2016-08-23 13:56:51 -0700

    need to use canvas instead of svg for links

--- js/fallingsnow_v6.js
+++ js/fallingsnow_v6.js
@@ -1,4 +1,8 @@
-﻿// The star of every good animation
+﻿//
+// To learn more about this effect, go to: //www.kirupa.com/html5/the_falling_snow_effect.htm
+//
+
+// The star of every good animation
 var requestAnimationFrame = window.requestAnimationFrame || 
                             window.mozRequestAnimationFrame || 
                             window.webkitRequestAnimationFrame ||
@@ -15,9 +19,9 @@ var transformProperty = getSupportedPropertyName(transforms);
 // Array to store our Snowflake objects
 var snowflakes = [];
 
-// Global variables to store our browser's window size
-var browserWidth;
-var browserHeight;
+// Global variable to store our target element and the element which will overlay it.
+var targetElement;
+var overlayElement;
 
 // Specify the number of snowflakes you want visible
 var numberOfSnowflakes = 50;
@@ -29,8 +33,8 @@ var resetPosition = false;
 // It all starts here...
 //
 function setup() {
-	window.addEventListener("DOMContentLoaded", generateSnowflakes, false);
-	window.addEventListener("resize", setResetFlag, false);
+    window.addEventListener("DOMContentLoaded", generateSnowflakes, false);
+    window.addEventListener("resize", setResetFlag, false);
 }
 setup();
 
@@ -49,22 +53,21 @@ function getSupportedPropertyName(properties) {
 //
 // Constructor for our Snowflake object
 //
-function Snowflake(element, radius, speed, xPos, yPos) {
+function Snowflake(element, speed, xPos, yPos) {
 
-	// set initial snowflake properties
+    // set initial snowflake properties
     this.element = element;
-    this.radius = radius;
     this.speed = speed;
     this.xPos = xPos;
     this.yPos = yPos;
-	
-	// declare variables used for snowflake's motion
+    
+    // declare variables used for snowflake's motion
     this.counter = 0;
     this.sign = Math.random() < 0.5 ? 1 : -1;
-	
-	// setting an initial opacity and size for our snowflake
-    this.element.style.opacity = .1 + Math.random();
-    this.element.style.fontSize = 12 + Math.random() * 50 + "px";
+    
+    // setting an initial opacity and size for our snowflake
+    this.opacity = .1 + Math.random();
+    this.fontSize = 12 + Math.random() * 50 + "px";
 }
 
 //
@@ -72,69 +75,64 @@ function Snowflake(element, radius, speed, xPos, yPos) {
 //
 Snowflake.prototype.update = function () {
 
-	// using some trigonometry to determine our x and y position
+    // using some trigonometry to determine our x and y position
     this.counter += this.speed / 5000;
     this.xPos += this.sign * this.speed * Math.cos(this.counter) / 40;
     this.yPos += Math.sin(this.counter) / 40 + this.speed / 30;
-
-	// setting our snowflake's position
-    setTranslate3DTransform(this.element, Math.round(this.xPos), Math.round(this.yPos));
     
-    // if snowflake goes below the browser window, move it back to the top
-    if (this.yPos > browserHeight) {
-    	this.yPos = -50;
+    // setting our snowflake's position
+    setPosition(this, Math.round(this.xPos), Math.round(this.yPos));
+    
+    // if snowflake goes below the bottom of the target element, move it back to the top
+    if (this.yPos > targetElement.clientHeight + 50) {
+        this.yPos = -50;
     }
 }
 
 //
-// A performant way to set your snowflake's position
+// A performant(?) way to set your snowflake's position
 //
-function setTranslate3DTransform(element, xPosition, yPosition) {
-	var val = "translate3d(" + xPosition + "px, " + yPosition + "px" + ", 0)";
-    element.style[transformProperty] = val;
+function setPosition(flake, x, y) {
+    var size = flake.fontSize;
+    flake.element.font = size + ' Cambria';
+    flake.element.fillText('.', x, y, 50);
+    flake.element.fillStyle = 'rgba(255, 255, 255, ' + flake.opacity + ')';
+}
+
+// position the overlay over its target
+function positionOverlay(){
+    var targetRect = targetElement.getBoundingClientRect();
+    overlayElement.style.top = targetRect.top;
+    overlayElement.style.left = targetRect.left;
+    overlayElement.setAttribute('width', targetRect.width);
+    overlayElement.setAttribute('height', targetRect.height);
 }
 
 //
 // The function responsible for creating the snowflake
 //
 function generateSnowflakes() {
-	
-	// get our snowflake element from the DOM and store it
-    var originalSnowflake = document.querySelector(".snowflake");
-    
-    // access our snowflake element's parent container
-    var snowflakeContainer = originalSnowflake.parentNode;
-    
-    // get our browser's size
-	browserWidth = document.documentElement.clientWidth;
-    browserHeight = document.documentElement.clientHeight;
-	        
-    // create each individual snowflake
-    for (var i = 0; i < numberOfSnowflakes; i++) {
-    
-    	// clone our original snowflake and add it to snowflakeContainer
-        var snowflakeCopy = originalSnowflake.cloneNode(true);
-        snowflakeContainer.appendChild(snowflakeCopy);
+    targetElement = document.querySelector('#content');
+    overlayElement = document.querySelector('#snowflakeContainer');
 
-		// set our snowflake's initial position and related properties
-        var initialXPos = getPosition(50, browserWidth);
-        var initialYPos = getPosition(50, browserHeight);
+    positionOverlay();
+                
+    // create each individual snowflake
+    for (var i = 0; i < numberOfSnowflakes; i++) {  
+        // set our snowflake's initial position and related properties
+        var initialXPos = getPosition(50, targetElement.clientWidth);
+        var initialYPos = getPosition(50, targetElement.clientHeight);
         var speed = 5+Math.random()*40;
-        var radius = 4+Math.random()*10;
         
         // create our Snowflake object
-        var snowflakeObject = new Snowflake(snowflakeCopy, 
-        									radius, 
-        									speed, 
-        									initialXPos, 
-        									initialYPos);
+        var snowflakeObject = new Snowflake(overlayElement.getContext('2d'), 
+                                            speed, 
+                                            initialXPos, 
+                                            initialYPos);
         snowflakes.push(snowflakeObject);
     }
     
-    // remove the original snowflake because we no longer need it visible
-	snowflakeContainer.removeChild(originalSnowflake);
-	
-	// call the moveSnowflakes function every 30 milliseconds
+    // call the moveSnowflakes function every 30 milliseconds
     moveSnowflakes();
 }
 
@@ -142,24 +140,24 @@ function generateSnowflakes() {
 // Responsible for moving each snowflake by calling its update function
 //
 function moveSnowflakes() {
+    var ctx = overlayElement.getContext('2d');
+    ctx.clearRect(0, 0, overlayElement.clientWidth, overlayElement.clientHeight);
     for (var i = 0; i < snowflakes.length; i++) {
         var snowflake = snowflakes[i];
         snowflake.update();
     }
     
-	// Reset the position of all the snowflakes to a new value
+    // Reset the position of all the snowflakes to a new value
     if (resetPosition) {
-    	browserWidth = document.documentElement.clientWidth;
-	    browserHeight = document.documentElement.clientHeight; 
-	    
-		for (var i = 0; i < snowflakes.length; i++) {
-	        var snowflake = snowflakes[i];
-	        
-	        snowflake.xPos = getPosition(50, browserWidth);
-	        snowflake.yPos = getPosition(50, browserHeight);
-	    }
-	    
-	    resetPosition = false;
+        positionOverlay();  
+        for (var i = 0; i < snowflakes.length; i++) {
+            var snowflake = snowflakes[i];
+            
+            snowflake.xPos = getPosition(50, targetElement.clientWidth);
+            snowflake.yPos = getPosition(50, targetElement.clientHeight);
+        }
+        
+        resetPosition = false;
     }
     
     requestAnimationFrame(moveSnowflakes);
@@ -169,12 +167,12 @@ function moveSnowflakes() {
 // This function returns a number between (maximum - offset) and (maximum + offset)
 //
 function getPosition(offset, size) {
-	return Math.round(-1*offset + Math.random() * (size+2*offset));
+    return Math.round(-1*offset + Math.random() * (size+2*offset));
 }
 
 //
 // Trigger a reset of all the snowflakes' positions
 //
 function setResetFlag(e) {
-	resetPosition = true;
+    resetPosition = true;
 }
